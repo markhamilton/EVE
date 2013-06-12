795b699238338db712087b6b1958b0cf96e51013
Mark Hamilton <evesoftware@gmail.com>
2013-06-12 10:51:39 -0500

    Folder structure, starting on premake script

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,4 @@
-#include "../include/simapp.hpp"
+#include "SimApp.hpp"
  
 #if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
---
+++ support/premake4.lua
@@ -0,0 +1,30 @@
+solution "EVE"
+	configurations { "Debug", "Release" }
+	debugdir "../debug"
+
+if not os.isdir("../debug") then os.mkdir("../debug") end
+
+--------------------------------------------------------------------------------
+-- EVE Flight Simulator 
+--------------------------------------------------------------------------------
+project "EVE"
+	uuid "D20CBF50-D363-11E2-8B8B-0800200C9A66"
+	language "C++"
+	includedirs { "../include" }
+	files { "../source/**" }
+	defines { "EVE_SIM" }
+	
+	configuration "Debug"
+		defines { "DEBUG" }
+		kind "WindowedApp"
+		flags { "Symbols" }
+		links { "OIS", "OGRE", "OGRE-Paging" }
+	
+	configuration "Release"
+		defines { "NDEBUG" }
+		kind "WindowedApp"
+		flags { "Optimize" }
+		links { "OIS", "OGRE", "OGRE-Paging" }
+
+
+-- TODO: Add paths for OGRE in the future
